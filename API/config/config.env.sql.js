7438d7e98cbf18e9d1edd75419f9888330153650
svend <sven.depickere@student.howest.be>
2019-10-19 17:55:41 +0200

    add SQL config

---
+++ API/config/config.env.sql.js
@@ -0,0 +1,14 @@
+/**
+ Created by svend on 19/10/2019.
+ **/
+
+module.exports = ({
+    client: 'mysql',
+    connection: {
+        host: "localhost",
+        port: 3306,
+        database: "happiness",
+        user: "root",
+        password: ""
+    }
+});
