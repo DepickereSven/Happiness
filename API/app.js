d46bc52981a5a40914f73028d88fded382701a45
svend <sven.depickere@student.howest.be>
2019-10-19 17:12:14 +0200

    add error response

--- API/app.js
+++ API/app.js
@@ -7,6 +7,13 @@ const indexRouter = require('./routes/index');
 
 app.use('/api/', indexRouter);
 
+app.use(function (req, res, next) {
+    res.status(404).json({
+        status: 404,
+        msg: "Sorry can't find that!"
+    })
+});
+
 app.listen(8001, function () {
     console.log('app listening at port %s', 8001);
 });
