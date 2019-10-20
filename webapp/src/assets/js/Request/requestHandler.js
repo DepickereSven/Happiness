860fe4523a93921fb2a35fb2fd20db65573bed88
svend <sven.depickere@student.howest.be>
2019-10-20 14:58:46 +0200

    added request to request handler

--- webapp/src/assets/js/Request/requestHandler.js
+++ webapp/src/assets/js/Request/requestHandler.js
@@ -2,12 +2,24 @@
  Created by svend on 20/10/2019.
  **/
 
+import dayStats from "./Get/Stats/requestForDay"
+import weekStats from "./Get/Stats/requestForWeek"
+import monthStats from "./Get/Stats/requestForMonth"
+import vote from "./Post/Vote/requestForVote"
+
 export default (function () {
 
-
+    const request = {
+        stats: {
+            day: dayStats.getTheStats,
+            week: weekStats.getTheStats,
+            month: monthStats.getTheStats
+        },
+        vote: vote.makeAVote
+    };
 
     return {
-        
+        request
     }
 
 })();
