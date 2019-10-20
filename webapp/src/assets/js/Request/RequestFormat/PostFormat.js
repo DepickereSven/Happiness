c2f13f11c8789d220a1681f150e12ab13189960b
svend <sven.depickere@student.howest.be>
2019-10-20 14:47:21 +0200

    added POST format request

---
+++ webapp/src/assets/js/Request/RequestFormat/PostFormat.js
@@ -0,0 +1,29 @@
+/**
+ Created by svend on 20/10/2019.
+ **/
+
+module.exports = (function () {
+
+    let fetchRequestForGettingData = function (fetchData) {
+        return fetch(fetchData.getSpecifiedElement ? fetchData.Url + fetchData.specifiedElement : fetchData.Url, {
+            method: fetchData.method,
+            body: fetchData.body
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return {error: true, msg: response.status, data: response};
+            }
+        }).then(json => {
+            return {error: false, msg: 'ok', data: json};
+        }).catch(function (error) {
+            console.log('Request failed ', error);
+            return {error: true, msg: '', data: error};
+        })
+    };
+
+    return {
+        fetchRequestForGettingData
+    }
+
+})();
