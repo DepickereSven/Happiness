f4eb87874f5d968525898cae30b90eef139e17da
svend <sven.depickere@student.howest.be>
2019-10-20 11:20:33 +0200

    added vote endpoint

--- API/routes/vote.js
+++ API/routes/vote.js
@@ -2,3 +2,27 @@
  Created by svend on 19/10/2019.
  **/
 
+const knex = require('knex')(require('../config/config.env.sql'));
+
+exports.vote = function (req, res) {
+    let data = req.body;
+    knex('votes').insert(
+        {
+            date: data.date,
+            happinessIndicator: data.happinessIndicator
+        })
+        .then(function (resp) {
+            res.json({
+                status: true,
+                message: 'ok'
+            })
+        })
+        .catch(function (error) {
+            res.json(
+                {
+                    status: false,
+                    msg: error.code
+                }
+            )
+        })
+};
