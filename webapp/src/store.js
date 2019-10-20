96520b5a3b364e7071678d8f24c624865185a05d
svend <sven.depickere@student.howest.be>
2019-10-20 17:23:09 +0200

    format code

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -5,12 +5,8 @@ import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {}
 })
