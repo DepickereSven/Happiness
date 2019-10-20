e7914e04fe26ac7f42fc782931d0c25b91337783
svend <sven.depickere@student.howest.be>
2019-10-20 14:53:52 +0200

    Create errorMessages.js

---
+++ webapp/src/assets/js/Request/errorMessages.js
@@ -0,0 +1,20 @@
+/**
+ Created by svend on 20/10/2019.
+ **/
+
+module.exports = (function () {
+
+    const messages = {
+        stats: {
+            day: "No stats available for the current day!",
+            week: "No stats available for the current week!",
+            month: "No stats available for the current month!"
+        },
+        vote: "Something went wrong, try again later!"
+    };
+
+    return {
+        messages
+    }
+
+})();
