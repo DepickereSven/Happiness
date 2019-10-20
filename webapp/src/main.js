b5ca64450566e84fc41b21287c7b75005f28cd4a
svend <sven.depickere@student.howest.be>
2019-10-20 14:25:41 +0200

    updated main with component

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import Default from './layouts/Default'
+import Empty from './layouts/Empty'
+
+Vue.component('default-layout', Default);
+Vue.component('empty-layout', Empty);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
