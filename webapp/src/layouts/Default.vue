1cc2a9cde0e7b897d0a530693f3b358408a0f6dd
svend <sven.depickere@student.howest.be>
2019-10-20 14:37:54 +0200

    update default layout

--- webapp/src/layouts/Default.vue
+++ webapp/src/layouts/Default.vue
@@ -1,5 +1,15 @@
 <template>
     <v-app id="inspire">
+        <v-app-bar app color="indigo" dark>
+            <v-toolbar-title>Happiness Indicator</v-toolbar-title>
+        </v-app-bar>
 
+        <v-content>
+            <slot></slot>
+        </v-content>
+
+        <v-footer color="indigo" app>
+            <span class="white--text">&copy; 2019</span>
+        </v-footer>
     </v-app>
 </template>
