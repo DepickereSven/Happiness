ddcf2c54429b6fba6720a5cc57e58a9266693768
svend <sven.depickere@student.howest.be>
2019-10-20 14:47:36 +0200

    added url module

---
+++ webapp/src/assets/js/Request/urlModule.js
@@ -0,0 +1,30 @@
+/**
+ Created by svend on 20/10/2019.
+ **/
+
+module.exports = (function () {
+
+    const IP = "localhost";
+    const PORT = "8001";
+    const BASE_URL = "http://" + IP + ":" + PORT + "/api/v1/";
+
+    const VOTE = "vote";
+    const STATS = "stats/";
+
+    const VOTE_URL = BASE_URL + VOTE;
+    const STATS_URL = BASE_URL + STATS;
+
+    const url = {
+        vote: VOTE_URL,
+        stats: {
+            day: STATS_URL + 'day',
+            week: STATS_URL + 'week',
+            month: STATS_URL + 'month',
+        }
+    };
+
+    return {
+        url
+    }
+
+})();
