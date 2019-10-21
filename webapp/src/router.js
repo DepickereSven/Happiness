d3d813e68a5c53e84a29e5111db5e1446942d8f2
svend <sven.depickere@student.howest.be>
2019-10-21 13:13:03 +0200

    adding Auth to router

--- webapp/src/router.js
+++ webapp/src/router.js
@@ -21,6 +21,17 @@ const router = new Router({
         path: '/stats',
         name: 'stats',
         component: () => import('./views/Stats.vue'),
+        meta: {
+          requiresAuth: true
+        }
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import('./views/Login.vue'),
+        meta: {
+          layout: "empty",
+        },
       },
       {
         path: '/404',
@@ -35,6 +46,24 @@ const router = new Router({
         redirect: '/404'
       }
     ]
+});
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = JSON.parse(localStorage.getItem('UH'));
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  if (isLoggedIn !== null) {
+    if (requiresAuth && !isLoggedIn.user) {
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    if (requiresAuth){
+      next('/');
+    } else {
+      next();
+    }
+  }
 });
 
 export default router
