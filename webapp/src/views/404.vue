76ef64f07956fb6f574dcd5dfde56d56d4eb2212
svend <sven.depickere@student.howest.be>
2019-10-21 16:05:21 +0200

    add styling 404

--- webapp/src/views/404.vue
+++ webapp/src/views/404.vue
@@ -1,5 +1,14 @@
 <template>
-    
+    <v-container fluid fill-height>
+        <v-layout align-center class="column-elements">
+
+            <Label class="display-4">Error 404</Label>
+
+
+            <v-btn x-large color="success" dark @click="$router.push({name: 'vote'})">Go Back</v-btn>
+
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
@@ -9,5 +18,10 @@
 </script>
 
 <style scoped>
+
+    .column-elements{
+        justify-content: space-evenly;
+    }
+
 
 </style>
