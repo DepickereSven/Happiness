ac1ce20fe74cd26fdcf29e050148165e7b5eeca6
svend <sven.depickere@student.howest.be>
2019-10-21 15:03:59 +0200

    Create requestToLogin.js

---
+++ webapp/src/assets/js/Request/Post/Login/requestToLogin.js
@@ -0,0 +1,28 @@
+/**
+ Created by svend on 21/10/2019.
+ **/
+
+import postRequest from "../../RequestFormat/PostFormat"
+import urlModule from "../../urlModule"
+import errorMessages from "../../errorMessages"
+
+export default (function () {
+
+    const makeAVote = async function (formData) {
+        const response = await postRequest.fetchRequestForGettingData({
+            Url: urlModule.url.login,
+            getSpecifiedElement: false,
+            method: "POST",
+            body: formData
+        });
+        if (response.error) {
+            response.msg = errorMessages.messages.vote;
+        }
+        return response;
+    };
+
+    return {
+        makeAVote
+    }
+
+})();
