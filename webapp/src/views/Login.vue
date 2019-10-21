5361f5b9576daaa2f6b8fc409bfe00c64e4a7e7c
svend <sven.depickere@student.howest.be>
2019-10-21 13:32:33 +0200

    Update Login.vue

---
+++ webapp/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app id="inspire">
+        <LoginForm></LoginForm>
+    </v-app>
+</template>
+
+
+<script>
+    import LoginForm from '../components/LoginForm'
+    export default {
+        components: {
+            LoginForm
+        }
+    }
+</script>
+
+<style scoped>
+</style>
