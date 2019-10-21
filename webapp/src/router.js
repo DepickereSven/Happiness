bf4c37bdbb8aeca228e8fad02e19941d8d178128
svend <sven.depickere@student.howest.be>
2019-10-21 13:20:02 +0200

    format code

--- webapp/src/router.js
+++ webapp/src/router.js
@@ -8,62 +8,62 @@ const router = new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
-      {
-        path: '/',
-        redirect: '/vote'
-      },
-      {
-        path: '/vote',
-        name: 'vote',
-        component: () => import('./views/Vote.vue'),
-      },
-      {
-        path: '/stats',
-        name: 'stats',
-        component: () => import('./views/Stats.vue'),
-        meta: {
-          requiresAuth: true
+        {
+            path: '/',
+            redirect: '/vote'
+        },
+        {
+            path: '/vote',
+            name: 'vote',
+            component: () => import('./views/Vote.vue'),
+        },
+        {
+            path: '/stats',
+            name: 'stats',
+            component: () => import('./views/Stats.vue'),
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('./views/Login.vue'),
+            meta: {
+                layout: "empty",
+            },
+        },
+        {
+            path: '/404',
+            name: '404',
+            component: () => import('./views/404.vue'),
+            meta: {
+                layout: "empty"
+            },
+        },
+        {
+            path: '*',
+            redirect: '/404'
         }
-      },
-      {
-        path: '/login',
-        name: 'login',
-        component: () => import('./views/Login.vue'),
-        meta: {
-          layout: "empty",
-        },
-      },
-      {
-        path: '/404',
-        name: '404',
-        component: () => import('./views/404.vue'),
-        meta: {
-          layout: "empty"
-        },
-      },
-      {
-        path: '*',
-        redirect: '/404'
-      }
     ]
 });
 
 router.beforeEach((to, from, next) => {
-  const isLoggedIn = JSON.parse(localStorage.getItem('UH'));
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-  if (isLoggedIn !== null) {
-    if (requiresAuth && !isLoggedIn.user) {
-      next('/');
+    const isLoggedIn = JSON.parse(localStorage.getItem('UH'));
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+    if (isLoggedIn !== null) {
+        if (requiresAuth && !isLoggedIn.user) {
+            next('/');
+        } else {
+            next();
+        }
     } else {
-      next();
+        if (requiresAuth) {
+            next('/');
+        } else {
+            next();
+        }
     }
-  } else {
-    if (requiresAuth){
-      next('/');
-    } else {
-      next();
-    }
-  }
 });
 
 export default router
