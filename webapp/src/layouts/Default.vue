e25535d0ada593b802972d21b24db34486ec9c4c
svend <sven.depickere@student.howest.be>
2019-10-21 13:19:53 +0200

    Update Default.vue

--- webapp/src/layouts/Default.vue
+++ webapp/src/layouts/Default.vue
@@ -3,7 +3,12 @@
         <v-app-bar app color="indigo" dark>
             <v-toolbar-title>Happiness Indicator</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-icon @click="$router.push({name: 'login'})">mdi-login-variant </v-icon>
+            <v-btn icon @click="$router.push({name: 'stats'})">
+                <v-icon>mdi-chart-areaspline-variant</v-icon>
+            </v-btn>
+            <v-btn icon @click="$router.push({name: 'login'})">
+                <v-icon>mdi-login-variant</v-icon>
+            </v-btn>
         </v-app-bar>
 
         <v-content>
