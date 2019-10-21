a9a507287b0841e00ec173de8a4d1957a9f79752
svend <sven.depickere@student.howest.be>
2019-10-21 15:08:45 +0200

    Create handle.js

---
+++ webapp/src/assets/js/Vue/Login/handle.js
@@ -0,0 +1,17 @@
+/**
+ Created by svend on 21/10/2019.
+ **/
+
+import requestHandler from "../../Request/requestHandler"
+
+export default (function () {
+
+    const init = function (_self) {
+
+    };
+
+    return {
+        init
+    }
+
+})();
