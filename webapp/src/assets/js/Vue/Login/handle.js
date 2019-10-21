0e802428a524892240e6f04a9af027ec0ae60a0a
svend <sven.depickere@student.howest.be>
2019-10-21 15:22:07 +0200

    handle to login

--- webapp/src/assets/js/Vue/Login/handle.js
+++ webapp/src/assets/js/Vue/Login/handle.js
@@ -6,12 +6,42 @@ import requestHandler from "../../Request/requestHandler"
 
 export default (function () {
 
-    const init = function (_self) {
-
+    const login = async function (_self) {
+        _self.isLoading = true;
+        await doTheLoginRequest(_self);
+        _self.isLoading = false;
     };
 
+    // ****** HELP FUNCTION ****** //
+
+    async function doTheLoginRequest(_self) {
+        let result = await requestHandler.request.login({
+            userName: _self.userName,
+            password: _self.password
+        });
+        if (result.error){
+            showAlertMessage(_self, result.msg);
+        } else {
+            saveToken(_self, result.data.token);
+            _self.$router.push({name: "stats"});
+        }
+    }
+
+    function showAlertMessage(_self, msg) {
+        _self.alert.status = true;
+        _self.alert.message = msg
+    }
+
+    function saveToken(_self, token) {
+        _self.$store.commit("updateLogin", {
+            user: true,
+            token: token
+        })
+    }
+
+
     return {
-        init
+        login
     }
 
 })();
