c45f0383feb1e2be30ada809ed1411ce9b55c435
svend <sven.depickere@student.howest.be>
2019-10-21 18:40:06 +0200

    add tooltips

--- webapp/src/layouts/Default.vue
+++ webapp/src/layouts/Default.vue
@@ -3,15 +3,30 @@
         <v-app-bar app color="indigo" dark>
             <v-toolbar-title>Happiness Indicator</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn icon @click="$router.push({name: 'vote'})">
-                <v-icon>mdi-vote</v-icon>
-            </v-btn>
-            <v-btn icon @click="$router.push({name: 'stats'})">
-                <v-icon>mdi-chart-areaspline-variant</v-icon>
-            </v-btn>
-            <v-btn icon @click="$router.push({name: 'login'})">
-                <v-icon>mdi-login-variant</v-icon>
-            </v-btn>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <v-btn icon @click="$router.push({name: 'vote'})" v-on="on">
+                        <v-icon>mdi-vote</v-icon>
+                    </v-btn>
+                </template>
+                <span>Vote</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <v-btn icon @click="$router.push({name: 'stats'})" v-on="on">
+                        <v-icon>mdi-chart-areaspline-variant</v-icon>
+                    </v-btn>
+                </template>
+                <span>Stats</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <v-btn icon @click="$router.push({name: 'login'})" v-on="on">
+                        <v-icon>mdi-login-variant</v-icon>
+                    </v-btn>
+                </template>
+                <span>Login</span>
+            </v-tooltip>
         </v-app-bar>
 
         <v-content>
